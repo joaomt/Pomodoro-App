5b35438f8a154328ca00eb8137f53792c51d9467
João Vitor[user] <joao_vitormt@hotmail.com>
2023-02-06 11:42:03 -0300

    feat: implement buildSrc

---
+++ buildSrc/src/main/kotlin/PomodoroVersions.kt
@@ -0,0 +1,11 @@
+import org.gradle.api.JavaVersion
+
+object PomodoroVersions {
+    const val compileSdk = 33
+    const val minSdk = 21
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    val javaCompileVersion = JavaVersion.VERSION_11
+}
